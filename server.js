49b74255eee4424374cfcf67a43540fc6fad558d
Freddie <119540226+SanJammin@users.noreply.github.com>
2026-03-28 15:26:35 +0000

    feat(api): return destination data from GET /api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/api" && req.method === "GET") {
-    res.end("This is from the server!");
+    const destinations = await getDataFromDB();
+    res.end(JSON.stringify(destinations));
   }
 });
 
