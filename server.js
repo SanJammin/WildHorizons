fee2fa3b77c875cf0a3092dc72ea575b6163b4c9
Freddie <119540226+SanJammin@users.noreply.github.com>
2026-03-30 22:19:50 +0100

    refactor(api): extract logic into utils

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 import http from "node:http";
 import { getDataFromDB } from "./database/db.js";
-import { error } from "node:console";
+import { sendJSONResponse } from "./utils/sendJSONResponse.js";
+import { send } from "node:process";
 
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
   if (req.url === "/api" && req.method === "GET") {
-    const destinations = await getDataFromDB();
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify(destinations));
+    sendJSONResponse(res, 200, destinations);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop();
     const filteredData = destinations.filter((destination) => {
       return destination.continent.toLowerCase() === continent.toLowerCase();
     });
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 200;
-    res.end(JSON.stringify(filteredData));
+    sendJSONResponse(res, 200, filteredData);
   } else {
-    res.statusCode = 404;
-    res.end(JSON.stringify({error: "Not found", message: "The requested route does not exist."}));
+    sendJSONResponse(res, 404, {error: "Not found", message: "The requested route does not exist."});
   }
 });
 
